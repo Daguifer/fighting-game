6716332d372d6a78a0522136d7940237d46e7fcb
Daguifer <davidcandas97@gmail.com>
2022-07-06 12:57:11 +0200

    Mejora Hitbox e Implementación de Barras de Vida

--- index.js
+++ index.js
@@ -1,203 +1,279 @@
 //Guardo el elemento canvas en una variable
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
 //Asigno el tamaño del canvas
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
 //Relleno el canvas, desde las posiciones 0 a la longitud del mismo
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
 
 //Creo la gravedad del canvas
-const gravity = 0.7
-
+const gravity = 0.7;
 
 //Se utiliza OOP para las entidades
-class Sprite{
-    constructor({position, velocity}) {
-        this.position = position
-        this.velocity = velocity
-        this.height = 150
-        this.lastKey
+class Sprite {
+  constructor({ position, velocity, color = "red", offset }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.width = 50;
+    this.height = 150;
+    this.lastKey;
+    this.isAttacking;
+
+    //Agrego la hitbox de los ataques
+    this.attackBox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      offset,
+      width: 100,
+      height: 50,
+    };
+
+    this.color = color;
+  }
+
+  //Defino el aspecto del jugador
+  draw() {
+    c.fillStyle = this.color;
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    //Implemento la hitbox de ataque
+    if (this.isAttacking) {
+      c.fillStyle = "green";
+      c.fillRect(
+        this.attackBox.position.x,
+        this.attackBox.position.y,
+        this.attackBox.width,
+        this.attackBox.height
+      );
     }
+  }
 
-    //Defino el aspecto del jugador
-    draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+  //Método para los updates de movimiento
+  update() {
+    this.draw();
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      //De esta manera evitamos que se caigan de la pantalla
+      this.velocity.y += gravity;
     }
+  }
 
-
-    //Método para los updates de movimiento
-    update(){
-        this.draw()
-        
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height){
-            this.velocity.y = 0
-        }else{
-            //De esta manera evitamos que se caigan de la pantalla
-            this.velocity.y += gravity
-        }
-    }
-
+  //Método de ataque
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100);
+  }
 }
 
 //Creo al jugador
 const player = new Sprite({
-   position:{
-        x: 0,
-        y: 0
-    },
+  position: {
+    x: 0,
+    y: 0,
+  },
 
-    velocity:{
-        x: 0,
-        y: 0
-    }
+  velocity: {
+    x: 0,
+    y: 0,
+  },
 
-
-})
+  offset: {
+    x: 0,
+    y: 0,
+  },
+});
 
 //Creo al enemigo
 const enemy = new Sprite({
-    
-    position:{
-        x: 400,
-        y: 100
-    },
+  position: {
+    x: 400,
+    y: 100,
+  },
 
-    velocity:{
-        x: 0,
-        y: 0
-    }
+  velocity: {
+    x: 0,
+    y: 0,
+  },
 
-})
+  offset: {
+    x: -50,
+    y: 0,
+  },
 
-console.log(player)
+  color: "blue",
+});
 
+console.log(player);
 
 //Afino la detección de teclas
 const keys = {
-    a:{
-        pressed: false
-    },
-    d:{
-        pressed: false
-    },
-    w:{
-        pressed: false
-    },
-    ArrowLeft:{
-        pressed: false
-    },
-    ArrowRight:{
-        pressed: false
-    },
-    ArrowUp:{
-        pressed: false
-    }
-}
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowUp: {
+    pressed: false,
+  },
+  ArrowDown: {
+    pressed: false,
+  },
+};
 
+//Colisión de las hitboxes
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+      rectangle2.position.x &&
+    rectangle1.attackBox.position.x <=
+      rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+      rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  );
+}
 
 //Creo la función para animar los Sprites
 function animate() {
-    window.requestAnimationFrame(animate)
-    //Limpio el canvas para que se dibujen correctamente
-    c.fillStyle = "black"
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    //Utilizo update en vez de draw para que vaya redibujandose
-    player.update()
-    enemy.update()
+  window.requestAnimationFrame(animate);
+  //Limpio el canvas para que se dibujen correctamente
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  //Utilizo update en vez de draw para que vaya redibujandose
+  player.update();
+  enemy.update();
 
-    //Para que no siga caminando solo
-    player.velocity.x = 0
-    enemy.velocity.x = 0
+  //Para que no siga caminando solo
+  player.velocity.x = 0;
+  enemy.velocity.x = 0;
 
-    //Checkeo si las teclas están pulsadas
-    //Update 1: nos quedamos con la ultima tecla pulsada
-    if(keys.a.pressed && player.lastKey === 'a'){
-        player.velocity.x = -5
-    }else if(keys.d.pressed && player.lastKey === 'd'){
-        player.velocity.x = 5
-    }
+  //Checkeo si las teclas están pulsadas
+  //Update 1: nos quedamos con la ultima tecla pulsada
+  if (keys.a.pressed && player.lastKey === "a") {
+    player.velocity.x = -5;
+  } else if (keys.d.pressed && player.lastKey === "d") {
+    player.velocity.x = 5;
+  }
 
-    //Movimiento del Enemigo/J2
-    if(keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft'){
-        enemy.velocity.x = -5
-    }else if(keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
-        enemy.velocity.x = 5
-    }
+  //Movimiento del Enemigo/J2
+  if (keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
+    enemy.velocity.x = -5;
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
+    enemy.velocity.x = 5;
+  }
 
+  //Detección de colisión
+  if (
+    rectangularCollision({ rectangle1: player, rectangle2: enemy }) &&
+    player.isAttacking
+  ) {
+    player.isAttacking = false;
+    document.querySelector('#enemyHealth').style.width ='20%'
+  }
+
+  if (
+    rectangularCollision({ rectangle1: enemy, rectangle2: player }) &&
+    enemy.isAttacking
+  ) {
+    enemy.isAttacking = false;
+    console.log("enemy attack successful");
+  }
 }
 
-animate()
-
+animate();
 
 //Voy a implementar el movimiento con event listeners
-window.addEventListener('keydown', (event) =>{
-    console.log(event.key)
-    switch(event.key){
-        case 'd':
-            keys.d.pressed = true
-            player.lastKey = 'd'
-            break
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      player.lastKey = "d";
+      break;
 
-        case 'a':
-            keys.a.pressed = true
-            player.lastKey = 'a'
-            break
+    case "a":
+      keys.a.pressed = true;
+      player.lastKey = "a";
+      break;
 
-        case 'w':
-            player.velocity.y = -20
-            break
+    case "w":
+      player.velocity.y = -20;
+      break;
 
+    case " ":
+      player.attack();
+      break;
 
-        //Teclas del Enemigo/J2
-        case 'ArrowRight':
-            keys.ArrowRight.pressed = true
-            enemy.lastKey = 'ArrowRight'
-            break
+    //Teclas del Enemigo/J2
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      enemy.lastKey = "ArrowRight";
+      break;
 
-        case 'ArrowLeft':
-            keys.ArrowLeft.pressed = true
-            enemy.lastKey = 'ArrowLeft'
-            break
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      enemy.lastKey = "ArrowLeft";
+      break;
 
-        case 'ArrowUp':
-            enemy.velocity.y = -20
-            break
-    }
-    console.log(event.key);
-})
+    case "ArrowUp":
+      enemy.velocity.y = -20;
+      break;
 
-window.addEventListener('keyup', (event) =>{
-    switch(event.key){
-        
-        case 'd':
-            keys.d.pressed = false
-            break
+    case "ArrowDown":
+      enemy.attack();
+      break;
+  }
+  console.log(event.key);
+});
 
-        case 'a':
-            keys.a.pressed = false
-            break
-        case 'w':
-            keys.w.pressed = false
-            break
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
 
-        //Teclas del Enemigo/J2
-        case 'ArrowRight':
-            keys.ArrowRight.pressed = false
-            break
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "w":
+      keys.w.pressed = false;
+      break;
 
-        case 'ArrowLeft':
-            keys.ArrowLeft.pressed = false
-            break
-        case 'ArrowUp':
-            keys.ArrowUp.pressed = false
-            break
-    }
-    console.log(event.key);
-})
+    //Teclas del Enemigo/J2
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
+    case "ArrowUp":
+      keys.ArrowUp.pressed = false;
+      break;
+    case "ArrowDown":
+      keys.ArrowDown.pressed = false;
+      break;
+  }
+});
